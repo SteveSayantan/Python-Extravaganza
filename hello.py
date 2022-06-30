4280018812a4114878a3f8d55b709e70c378d81d
Steve <sayantanmanna06@gmail.com>
2022-06-30 16:04:59 +0530

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+# Single Line Comment
+
+'''Multi 
+Line 
+Comments are written like this'''
+
+print("hello world!!") #Prints hello world and adds a new line at the end
+
+print("This is a Statement",end="") #Prints the String but does not add a new line at the end (We can also use " "(blank space), "," (comma), "Steve" etc. Default value of end is "\n" (new line character))
+print("This is another line")
+
+print('Today is Thursday','Tomorrow is Friday') #Adds a space between two strings
+
+print('This Mango is "Sweet"')
+print('This Mango is \'Sweet\'')
+
+print('Today is Thursday\n\t','Tomorrow is Friday') # \n starts a new line and \t gives a tab
